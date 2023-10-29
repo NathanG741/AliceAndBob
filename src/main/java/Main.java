8127306e1b4a7d2f01bec5384db81d17752207eb
nathan <nathan.guardado@yahoo.com>
2023-10-29 19:40:08 -0400

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        String userInput = getStringInput("What's your name?");
+        if (userInput.equals("Alice")){
+            System.out.println("Hello Alice");
+        }
+        else if (userInput.equals("Bob")){
+            System.out.println("Hello Bob");
 
+        }
+         else {
+            System.out.println("Go away " + userInput);
+        }
+    }
+    public static String getStringInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
+        String userInput = scanner.nextLine();
+        return userInput;
     }
 }
